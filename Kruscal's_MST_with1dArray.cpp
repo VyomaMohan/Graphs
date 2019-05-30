dadc75c328ece374aae00bf61f77f3af1fe937a2
VyomaMohan <46680968+VyomaMohan@users.noreply.github.com>
2019-05-30 13:40:05 +0530

    Update Kruscal's_MST_with1dArray.cpp

--- Kruscal's_MST_with1dArray.cpp
+++ Kruscal's_MST_with1dArray.cpp
@@ -11,6 +11,7 @@ struct edge{
 };
 typedef struct edge EDGE;
 
+//vertices numberes from 0-8 and stored
 EDGE edge_list[NOE]={{1,6,1},{2,3,2},{0,2,3},{1,4,3},{5,6,4},{0,3,5},{4,8,5},{7,8,6},{3,6,6},{2,5,9},{0,1,9},{6,8,10},{5,7,12},{6,4,15}};
 //EDGE edge_list[NOE]={{0,1,1},{0,2,1},{1,3,1},{0,3,2},{2,3,3},{1,2,4}};
 
